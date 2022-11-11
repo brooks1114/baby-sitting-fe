13d8eb00791931a66f506853108252f78fad80b0
brooks1114 <58515991+brooks1114@users.noreply.github.com>
2022-11-11 15:51:23 -0500

    added cards for sitter returned results

--- src/pages/sitters/Sitters.js
+++ src/pages/sitters/Sitters.js
@@ -1,14 +1,15 @@
 import Header from "../../components/header/Header";
 import Slideshow from "../../components/slideshow/Slideshow";
-import GridCards from "../../components/gridcards/GridCards";
+import GridCardsSitters from "../../components/sitters/GridCardsSitters";
+import SittersSearchResults from "../../components/sitters/SittersSearchResults";
 import { isAuthenticated } from "../../utils/authHelper";
 
 function Home(props) {
   return (
     <div className="Home">
-        <Header isAuthenticated={isAuthenticated()} />
-        <Slideshow/>
-        <GridCards/>
+      <Header isAuthenticated={isAuthenticated()} />
+      <Slideshow />
+      <SittersSearchResults />
     </div>
   );
 }
